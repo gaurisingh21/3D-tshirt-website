909d0037ab19fa4e172dbfa7fbcd03708718c8b4
Gauri Singh <gaurivinsha@gmail.com>
2024-01-14 15:00:56 +0100

    Final commit

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,10 +3,10 @@ import { proxy } from 'valtio';
 const state = proxy({
     intro: true,
     color: '#EFBD48',
-    isLogoTexture: false,
+    isLogoTexture: true,
     isFullTexture: false,
     logoDecal: './threejs.png',
-    fullDecal: './threejs.png'
+    fullDecal: './threejs.png',
 });
 
 export default state;
